6e72d1edde8741bb18e2f6787b1de4e9e5d25329
khendrych <khendrych@juniper.net>
2024-11-12 01:50:58 +0100

    Update template-ops.py

--- template-ops.py
+++ template-ops.py
@@ -4,7 +4,7 @@ ver = "2024-09-29.138"
 """
 template-ops tool 
 
-Copyright 2024 Karel Hendrych khendrych@juniper.net
+Copyright (c) 2024, Juniper Networks, Inc. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
